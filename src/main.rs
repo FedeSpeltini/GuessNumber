481375bdf4dabc293b5645b47ec17a103c263be6
Federico Speltini <federico.speltini@bankingly.com>
2024-01-25 17:18:20 -0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,10 @@ enum ValidationResult {
     Correct,
 }
 
-fn parse_number(number : &str) -> i32 {
-    let parsed = number.trim().parse::<i32>()
-    .expect("Por favor, escribe un número válido");
-    parsed
+fn parse_number(number: &str) -> Result<i32, std::num::ParseIntError> {
+    number.trim().parse::<i32>()
 }
+
 
 fn validate_number(number_input: i32, number_guess: i32) -> ValidationResult {
     if number_input > number_guess {
@@ -24,20 +23,32 @@ fn validate_number(number_input: i32, number_guess: i32) -> ValidationResult {
 fn main() {
     println!("Adivina el número!");
     println!("Los números válidos son del 1 al 1000");
-    println!("Escribe 'Q' para salir del juego");
+    println!("Escribe 'exit' para salir del juego");
     let number_guess = rand::thread_rng().gen_range(1..=1000);
     loop{
         let mut guess = String::new();
         std::io::stdin().read_line(&mut guess).unwrap();
-        if guess.trim() != "Q" {
-            match validate_number(parse_number(&guess), number_guess) {
-                ValidationResult::Lower => println!("El número es menor"),
-                ValidationResult::Higher => println!("El número es mayor"),
-                ValidationResult::Correct => {
-                    println!("¡Adivinaste!");
-                    break;
+        if guess.trim().to_lowercase() == "quit" {
+
+            match parse_number(&guess) {
+                Ok(number) => {
+
+                    match validate_number(number, number_guess) {
+                        ValidationResult::Lower => println!("El número es menor"),
+                        ValidationResult::Higher => println!("El número es mayor"),
+                        ValidationResult::Correct => {
+                            println!("¡Adivinaste!");
+                            break;
+                        }
+                    }
+
+                },
+                Err(err) => {
+                    println!("Error al analizar el número: {}", err);
                 }
             }
+
+
         }
         else {
             break;
